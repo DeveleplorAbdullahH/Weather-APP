40927cbb71fa8f10d9d01650678f627a5a0be9ef
DeveleplorAbdullahH <128960004+DeveleplorAbdullahH@users.noreply.github.com>
2023-08-18 00:05:30 +0400

    Add files via upload

---
+++ process.php
@@ -0,0 +1,7 @@
+<?php
+// Get the form data.
+$name = $_POST["name"];
+$email = $_POST["email"];
+$amount = $_POST["amount"];
+// Set up email headers and message body for confirmation mail to user.
+?>
